9ec8d84e8e6914cbf30e703632608877854f53be
Lee Martin <leefredmart@gmail.com>
2019-11-04 12:23:50 -0500

    first commit

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID: "1025432699012-ujtfhfsdh86d1mnt3dmcoo6s133h9iq3.apps.googleusercontent.com",
+        clientSecret: "5xjZSHf2EyQTC4fdTWJDEmEw"
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://Lee:Crusher79!@cluster0-efaxd.mongodb.net/OAuth?retryWrites=true'
+    },
+    session: {
+        cookieKey: 'I like kdlskjdsk babies'
+    }
+}
